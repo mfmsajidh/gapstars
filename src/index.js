ef4c9de3eb3bff98d6a61f31e61f56fe55449abc
Sajidh Farook <sajidh@begood.solutions>
2022-11-09 01:01:06 +0530

    Added comments for understandability

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import parseArgs from 'minimist';
 import axios from "axios";
 import mergeImg from 'merge-img';
 
+// Gets values from the command line arguments
 const {greeting, who, width, height, color, size} = parseArgs(process.argv.slice(2));
 
+// Generates an axios instance to get images
 const getImage = (newGreeting) => {
     return axios({
         method: 'get',
@@ -12,11 +14,13 @@ const getImage = (newGreeting) => {
     })
 }
 
+// Runs concurrent requests to get the necessary images
 Promise.all([getImage(greeting), getImage(who)])
     .then(async (results) => {
         const image1 = results[0].data;
         const image2 = results[1].data;
 
+        // Merges the two images after both the requests are completed
         await mergeImg([image1, image2])
             .then((img) => {
                 img.write('cat-card.jpg', () => console.log("The file was saved!"));
